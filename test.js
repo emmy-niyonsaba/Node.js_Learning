7562e65bc3ac5b523e2cce109ba6223dce390b58
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-04-14 16:49:52 +0200

    chore: done by updata and delete  with fs module

--- test.js
+++ test.js
@@ -20,3 +20,23 @@ const write =()=>{
 });
 }
 // write()
+const update =()=>{
+    fs.appendFile('./docs/blog1.txt', ' This is an appended text.', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }   
+    console.log('File updated successfully!');
+});
+}
+// update()
+const deleteFile =()=>{
+    fs.unlink('./docs/blog1.txt', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('File deleted successfully!');
+});
+}
+// deleteFile()     

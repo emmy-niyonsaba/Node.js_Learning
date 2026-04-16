6196c53a0cc609adf8934e50961d7dce177fad48
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-04-16 23:06:18 +0200

    add the style in blogs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 // app.set('views', __dirname + '/views');

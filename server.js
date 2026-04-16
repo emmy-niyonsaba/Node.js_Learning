4c42f4544ad5cfbed476cbbef4036d099525198d
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-04-16 23:48:18 +0200

    done by integration the mogoose db  and connect

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const port = 3000;
+const dbURI = 'mongodb+srv://emmyson:<emmy123!@#>@cluster0.fgvybye.mongodb.net/';
+mongoose.connect(dbURI)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Example app listening at port:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
@@ -23,9 +37,17 @@ app.get('/about', (req, res) => {
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
+
+
+
+app
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at port:${port}`);
 });

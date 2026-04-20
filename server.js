5a33b040f9c10a445d60c25db024aaf134efaab5
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-04-20 11:20:32 +0200

    done by getall and getsingleby id

--- server.js
+++ server.js
@@ -23,74 +23,9 @@ mongoose.connect(dbURI)
   });
 // this way to set the public foler for node while it is running 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
-
-
-
-const blogs = [
-  { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-  { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-  { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
-]
-app.get('/add-blog', (req, res) => {
-  if (mongoose.connection.readyState !== 1) {
-    return res.status(503).send('Database not connected. Check MONGODB_URI and MongoDB network access.');
-  }
-
-  const blog = new Blog({
-    title: 'new blog 2',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
-  });
-
-  blog.save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Failed to save blog');
-    });
-  });
-
-  app.get('/all-blogs', (req, res) => {
-    if (mongoose.connection.readyState !== 1) {
-      return res.status(503).send('Database not connected. Check MONGODB_URI and MongoDB network access.');
-    }
-    Blog.find()
-      .then((blogs) => {
-        res.send(blogs);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Failed to fetch blogs');
-      });
-  });
-
-
-  app.get('/single-blog', (req, res) => {
-    if (mongoose.connection.readyState !== 1) {
-      return res.status(503).send('Database not connected. Check MONGODB_URI and MongoDB network access.');
-    }
-    Blog.findById('69e5e64ee428334398bc5e60') 
-      .then((blog) => {
-        res.send(blog);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Failed to fetch blog');
-      })
-      });
-
-
-
-
-
-
-
-
 
 app.get('/', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
@@ -107,6 +42,30 @@ app.get('/about', (req, res) => {
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog.save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Failed to save blog');
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((blog) => {
+      res.render('details', { title: blog.title, blog: [blog] });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Failed to fetch blog');
+    });
+});
+
 
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });

32fc75f3a4978408ce53476a2a2ef19e6d124042
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-04-20 10:56:07 +0200

    done with rendering the real blog from database

--- server.js
+++ server.js
@@ -54,12 +54,56 @@ app.get('/add-blog', (req, res) => {
       res.status(500).send('Failed to save blog');
     });
   });
+
+  app.get('/all-blogs', (req, res) => {
+    if (mongoose.connection.readyState !== 1) {
+      return res.status(503).send('Database not connected. Check MONGODB_URI and MongoDB network access.');
+    }
+    Blog.find()
+      .then((blogs) => {
+        res.send(blogs);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Failed to fetch blogs');
+      });
+  });
+
+
+  app.get('/single-blog', (req, res) => {
+    if (mongoose.connection.readyState !== 1) {
+      return res.status(503).send('Database not connected. Check MONGODB_URI and MongoDB network access.');
+    }
+    Blog.findById('69e5e64ee428334398bc5e60') 
+      .then((blog) => {
+        res.send(blog);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Failed to fetch blog');
+      })
+      });
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.render('home', { title: 'Home', blogs: blogs });
+  Blog.find().sort({ createdAt: -1 })
+    .then((blogs) => {  
+  res.render('home', { title: 'All Blogs', blogs: blogs });
 });
+})
+
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
+
+//blogs router
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
